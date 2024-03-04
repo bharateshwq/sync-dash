8ca539ace763fd90041c1bdd7e92d091b0316330
bharateshwq <breditter@gmail.com>
2024-03-04 21:07:48 +0530

    fixed and update react dependencies

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2020 All rights reserved by bharatesh.carrd.co
+    </p>
+  </div>
+);
+
+export default Footer;
